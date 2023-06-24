d0cbef13b306e40c3d88fa1f120d44adfba0a667
Samuel Seo <ngogiaphat0802@gmail.com>
2023-06-24 20:10:32 +0700

    create database and run test database

---
+++ base/migrations/0003_user_avatar.py
@@ -0,0 +1,12 @@
+from django.db import migrations, models
+class Migration(migrations.Migration):
+	dependencies = [
+		('base', '0002_auto_20210921_1315'),
+	]
+	operations = [
+		migrations.AddField(
+			model_name='user',
+			name='avatar',
+			field=models.ImageField(default='avatar.svg', null=True, upload_to=''),
+		),
+	]
